d430d6b3dd3853091a077eed54b4848c1af2eb85
HyeonLo <ks12467@naver.com>
2024-12-17 19:51:23 +0900

    1주차 숙제 - 요리 레시피 메모장을 만들어보자

---
+++ src/Java_First_Week_HomeWork.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Java_First_Week_HomeWork {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String title = sc.nextLine();
+
+        double score = sc.nextDouble();
+
+        String recipe1 = sc.nextLine();
+        String recipe2 = sc.nextLine();
+        String recipe3 = sc.nextLine();
+        String recipe4 = sc.nextLine();
+        String recipe5 = sc.nextLine();
+        String recipe6 = sc.nextLine();
+        String recipe7 = sc.nextLine();
+        String recipe8 = sc.nextLine();
+        String recipe9 = sc.nextLine();
+        String recipe10 = sc.nextLine();
+
+        System.out.println(title);
+        System.out.println(score);
+        System.out.println(recipe1);
+        System.out.println(recipe2);
+        System.out.println(recipe3);
+        System.out.println(recipe4);
+        System.out.println(recipe5);
+        System.out.println(recipe6);
+        System.out.println(recipe7);
+        System.out.println(recipe8);
+        System.out.println(recipe9);
+        System.out.println(recipe10);
+    }
+}
