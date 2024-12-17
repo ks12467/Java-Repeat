2aa3865dea22489db72a11b298cad34e214466ac
HyeonLo <ks12467@naver.com>
2024-12-17 19:38:51 +0900

    아스키코드와 형변환

--- src/Java_First_Week_1.java
+++ src/Java_First_Week_1.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Java_First_Week_1 {
     public static void main(String[] args) {
@@ -45,5 +46,59 @@ public class Java_First_Week_1 {
         Integer num = number;
 
         System.out.println(num.intValue());
+
+        //숫자 - > 문자 (아스키코드)
+        Scanner sc = new Scanner(System.in);
+        int asciiNumber = sc.nextInt();
+        char ch = (char)asciiNumber;
+
+        System.out.println(ch);
+
+        //문자 -> 숫자(아스키코드)
+        Scanner sc1 = new Scanner(System.in);
+        char letter = sc1.nextLine().charAt(0);
+        int asciiNumber2 = (int)letter;
+
+        System.out.println(asciiNumber2);
+
+        //형변환 double or float
+        double doubleNumber1 = 10.101010;
+        float floatNumber1 = 10.1010f;
+
+        int intNumber1;
+        intNumber1 = (int)doubleNumber1;
+        System.out.println(intNumber1);
+
+        int intNumber2;
+        intNumber2 = (int)floatNumber1;
+        System.out.println(intNumber2);
+
+        //형변환 정수형
+        int intNumber3 = 10;
+        double doubleNumber2 = (double)intNumber3;
+        float floatNumber2 = (float)intNumber3;
+
+        System.out.println(doubleNumber2);
+        System.out.println(floatNumber2);
+
+        //변수 타입별 크기 순서
+        //byte -> short -> int -> long -> float -> double
+        byte byteNumber1 = 10;
+        int intNumber4 = byteNumber1;
+        System.out.println(intNumber4);
+
+        char charAlphabet = 'A';
+        intNumber4 = charAlphabet;
+        System.out.println(intNumber4);
+
+        intNumber4 = 100;
+        long longNumber1 = intNumber4;
+        System.out.println(longNumber1);
+
+        intNumber4 = 200;
+        double doubleNumber3 = intNumber4;
+        System.out.println(doubleNumber3);
+
+
     }
 }
