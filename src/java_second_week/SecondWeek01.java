58e0c552976894dcbe383ea14e746dc9d48e0c3f
HyeonLo <ks12467@naver.com>
2024-12-18 15:31:29 +0900

    사칙연산

---
+++ src/java_second_week/SecondWeek01.java
@@ -0,0 +1,27 @@
+package java_second_week;
+
+public class SecondWeek01 {
+    public static void main(String[] args) {
+        //x + y
+        int x = 5;
+        int y = 10;
+        int result;
+        result = x + y;
+        System.out.println(result);
+
+        //x - y
+        result = x - y;
+        System.out.println(result);
+
+        //x * y
+        result = x * y;
+        System.out.println(result);
+
+        //x \ y
+        result = x / y;
+        System.out.println(result);
+
+        result = y % x;
+        System.out.println(result);
+    }
+}
