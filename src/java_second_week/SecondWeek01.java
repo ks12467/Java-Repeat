82e0e3768a6c150bf634af0ab95475a85ea13fc2
HyeonLo <ks12467@naver.com>
2024-12-18 15:57:44 +0900

    비교연산자, 논리연산자

--- src/java_second_week/SecondWeek01.java
+++ src/java_second_week/SecondWeek01.java
@@ -23,5 +23,39 @@ public class SecondWeek01 {
 
         result = y % x;
         System.out.println(result);
+
+        System.out.println("-------------------------");
+
+        //비교 연산
+        System.out.println(10>9);
+        System.out.println(12 >=9);
+        System.out.println(10<9);
+        System.out.println(10<=9);
+        System.out.println(10==9);
+        System.out.println(10!=9);
+        System.out.println(10 == 10);
+
+        System.out.println("-------------------------");
+        //논리연산자
+        boolean flag1 = true;
+        boolean flag2 = true;
+        boolean flag3 = false;
+
+        System.out.println(flag1);
+        System.out.println(flag2);
+        System.out.println(flag3);
+
+        System.out.println("OR-------------------------");
+
+        System.out.println(flag1 || flag2 );
+        System.out.println(flag1 || flag2 || flag3);
+
+        System.out.println("AND------------------------");
+
+        System.out.println(flag1 && flag2);
+        System.out.println(flag1 && flag2 && flag3);
+
+        System.out.println("!-------------------------");
+        System.out.println(!flag1);
     }
 }
